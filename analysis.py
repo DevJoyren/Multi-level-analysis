35a16dad9ae616af165059be8494224f7d1fcbbc
DevJoyren <devjoyren@gmail.com>
2025-10-09 00:41:38 +0200

    Initial commit +  basic structures

---
+++ analysis.py
@@ -0,0 +1,53 @@
+# Imports >>>
+
+
+
+class Analysis:
+    def __init__(self):
+        self.total_words = 0
+        ...
+
+
+    def text_input(self, file_path):
+        """
+        Handles the text input from the user.
+        :param file_path:
+        :return:
+        """
+        ...
+
+
+    def sentence_analyser(self, text):
+        """
+         Analyses the sentences and handles every requirement.
+        :param text:
+        :return:
+        """
+        ...
+
+
+    def word_analyser(self, text):
+        """
+        Analyses the words and handles every requirement.
+        :param text:
+        :return:
+        """
+        ...
+
+
+    def generate_report(self, stats):
+        """
+        Generates a report from the given statistics.
+        :param stats:
+        :return:
+        """
+        ...
+
+
+    def save_report(self, output_path):
+        """
+
+        :param output_path:
+        :return:
+        """
+        ...
---
+++ main.py
@@ -0,0 +1,7 @@
+from analysis import Analysis
+
+if __name__ == '__main__':
+    analysis = Analysis()
+
+    # more to come we called the Analysis() class
+    # now using analysis.  makes us able to run functions from Analysis()
