9f5568ef6965d51d4629be2b6bd2925b9a4d6586
DevJoyren <devjoyren@gmail.com>
2025-10-17 00:57:46 +0200

    minor variable chhanges and comment changes. some polishing still needs more tho...

--- analysis.py
+++ analysis.py
@@ -17,7 +17,6 @@ class Analysis:
         self.word_len = Counter()
 
 
-
         # All stats together for creating final report.
         self.stats = {}
 
@@ -26,7 +25,7 @@ class Analysis:
         """
         Handles the text input from the user.
         :param file_path:
-        :return:
+        :return: text
         """
         with open(file_path, 'r') as file:
             text = file.read()   # Read the file
@@ -47,8 +46,6 @@ class Analysis:
 
 
 
-
-
     def word_analyser(self, text:str) -> None :
         """
         :param text:
@@ -59,13 +56,12 @@ class Analysis:
          - vocabulary richness (unique/total in percentage
          - word length
          - word count
-         - rare words (appear exactly once)
+         - hapax words (appear exactly once)
          - top 20 words
         """
 
         # Matches any single "word" character (alphanumeric or underscore). Equivalent to [a-zA-Z0-9_].
         # self.words is a list containing all words inside the document
-        # including duplicates
         self.words = re.findall(r"[a-zA-Z0-9]+(?:'[a-zA-Z0-9]+)?", text)
 
         # The length of the words list equals to the number of words in numbers
@@ -80,7 +76,6 @@ class Analysis:
         vocabulary_size = len(self.unique_words)
         richness = (vocabulary_size / self.total_words) * 100 if self.total_words else 0.0
 
-
         # Counts each character inside all words and organizes them based on amount of characters (length)
         # Also gets the most common length
         self.word_len = Counter(len(w) for w in self.words if w)
@@ -88,7 +83,6 @@ class Analysis:
 
         # find the hapax inside the given text.
         # Hapax is a word OR phrase that appears only once in a given body of text
-        # Source: google AI headline
         hapax_count = sum(1 for w, c in self.unique_words.items() if c == 1)
 
         # find the top 20 most words inside the text and counts them
@@ -98,9 +92,8 @@ class Analysis:
         top20_percentage = (top20_total / self.total_words) * 100 if self.total_words else 0.0
 
 
-
-        # update the statistics list with their name and corresponding value.
-        # Easy saves everything inside a list
+        # Update the statistics list with their name and corresponding value.
+        # Easily saves everything inside a list
         self.stats.update({
             "total_words": self.total_words,
             "unique_words": vocabulary_size,
@@ -108,18 +101,12 @@ class Analysis:
             "most_common_word_length": most_common_word_len,
             "hapax_count": hapax_count,
             "top20_words": top20,
-            "top20_percentage": round(top20_percentage, 2),
+            "top20_percentage": round(top20_percentage, 2), # 2 decimals
 
         })
 
         # test with printing out the list of statistics
         print(self.stats)
-
-
-
-
-
-
 
 
 
